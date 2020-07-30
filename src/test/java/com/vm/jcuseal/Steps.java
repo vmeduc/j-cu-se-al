055fe059131852fa9aef6291f90e8f39c08d7133
Vlad Melekhin <vmelekhin@wsoft.ru>
2020-07-30 18:50:19 +0300

    добавил пару сценариев; начал вводить web-driver-manager

--- src/test/java/com/vm/jcuseal/Steps.java
+++ src/test/java/com/vm/jcuseal/Steps.java
@@ -1,11 +1,37 @@
 package com.vm.jcuseal;
 
+import com.codeborne.selenide.Condition;
 import io.cucumber.java.ru.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
-import static org.junit.Assert.assertTrue;
+import static com.codeborne.selenide.Selenide.*;
 
 public class Steps {
 
+    private WebDriver driver;
 
+    public Steps() {
 
+    }
+
+    @Дано("открыта главная страница Яндекс")
+    public void открытаГлавнаястраницаЯндекс() {
+        open("https://yandex.ru");
+    }
+
+    @Когда("пользователь вводит запрос {string}")
+    public void пользовательВводитЗапрос(String query) {
+        $(By.id("text")).setValue(query);
+    }
+
+    @И("нажимает Найти")
+    public void нажимаетНайти() {
+        $(By.xpath("//button")).click();
+    }
+
+    @Тогда("на странице есть {string}")
+    public void наСтраницеЕсть(String text) {
+        $(By.xpath("//div[@class='entity-search__header']")).shouldHave(Condition.text(text));
+    }
 }
