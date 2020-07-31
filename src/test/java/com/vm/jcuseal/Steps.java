2e6d1771710b5ded0fa2b778173d25eb401941b4
Vlad Melekhin <vmelekhin@wsoft.ru>
2020-07-31 15:56:26 +0300

    Перевел с Selenium на Selenide (WebDriverManager присутствует)

--- src/test/java/com/vm/jcuseal/Steps.java
+++ src/test/java/com/vm/jcuseal/Steps.java
@@ -1,16 +1,22 @@
 package com.vm.jcuseal;
 
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeStep;
-import io.cucumber.java.ru.*;
+import io.cucumber.java.ru.Дано;
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.Тогда;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import jdk.jfr.StackTrace;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class Steps {
 
@@ -18,14 +24,19 @@ public class Steps {
 
     @Before
     public void setupClass() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(500, 500));
+
+        WebDriverRunner.setWebDriver(driver);
 
         System.out.println("Before");
     }
 
     @BeforeStep
     public void setupTest() {
+
         System.out.println("Before step");
     }
 
@@ -38,26 +49,22 @@ public class Steps {
 
     @Дано("открыта главная страница Яндекс")
     public void открытаГлавнаястраницаЯндекс() {
-        driver.get("https://yandex.ru");
+        open("https://yandex.ru");
     }
 
     @Когда("пользователь вводит запрос {string}")
     public void пользовательВводитЗапрос(String query) {
-        driver.findElement(By.id("text")).sendKeys(query);
+        $(By.id("text")).setValue(query);
     }
 
     @И("нажимает Найти")
     public void нажимаетНайти() {
-        driver.findElement(By.xpath("//button")).click();
+        $(By.xpath("//button")).click();
     }
 
     @Тогда("на странице есть {string}")
     public void наСтраницеЕсть(String text) {
-        try {
-            driver.findElement(By.xpath("//div[text()='огурецы']"));
-        } catch (Throwable e) {
-            Assert.assertTrue(" Текст \"" + text + "\" не найден: " + e.getMessage(), false);
-            e.printStackTrace();
-        }
+        boolean exists = $(By.xpath("//b[contains(text(),'cucumber.io')]")).exists();
+        Assert.assertTrue("огурцов нет", exists);
     }
 }
