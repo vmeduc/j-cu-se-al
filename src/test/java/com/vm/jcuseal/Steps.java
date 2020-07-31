eaf61732090188428127f22f62c2c3cb93388ea6
Vlad Melekhin <vmelekhin@wsoft.ru>
2020-07-31 13:38:00 +0300

    Наладил работу WebDriverManager и Selenium

--- src/test/java/com/vm/jcuseal/Steps.java
+++ src/test/java/com/vm/jcuseal/Steps.java
@@ -1,37 +1,63 @@
 package com.vm.jcuseal;
 
 import com.codeborne.selenide.Condition;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 import io.cucumber.java.ru.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import jdk.jfr.StackTrace;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
-import static com.codeborne.selenide.Selenide.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Steps {
 
     private WebDriver driver;
 
-    public Steps() {
+    @Before
+    public void setupClass() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
 
+        System.out.println("Before");
+    }
+
+    @BeforeStep
+    public void setupTest() {
+        System.out.println("Before step");
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Дано("открыта главная страница Яндекс")
     public void открытаГлавнаястраницаЯндекс() {
-        open("https://yandex.ru");
+        driver.get("https://yandex.ru");
     }
 
     @Когда("пользователь вводит запрос {string}")
     public void пользовательВводитЗапрос(String query) {
-        $(By.id("text")).setValue(query);
+        driver.findElement(By.id("text")).sendKeys(query);
     }
 
     @И("нажимает Найти")
     public void нажимаетНайти() {
-        $(By.xpath("//button")).click();
+        driver.findElement(By.xpath("//button")).click();
     }
 
     @Тогда("на странице есть {string}")
     public void наСтраницеЕсть(String text) {
-        $(By.xpath("//div[@class='entity-search__header']")).shouldHave(Condition.text(text));
+        try {
+            driver.findElement(By.xpath("//div[text()='огурецы']"));
+        } catch (Throwable e) {
+            Assert.assertTrue(" Текст \"" + text + "\" не найден: " + e.getMessage(), false);
+            e.printStackTrace();
+        }
     }
 }
